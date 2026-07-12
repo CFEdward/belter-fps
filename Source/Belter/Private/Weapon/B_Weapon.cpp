7de1c43859e7e55887ef21abcdaf353fea66f7b0
Eduard Ciofu <cfedward7@gmail.com>
2026-07-13 00:26:22 +0200

    weapon

---
+++ Source/Belter/Private/Weapon/B_Weapon.cpp
@@ -0,0 +1,31 @@
+﻿// Copyright Eduard Ciofu
+
+#include "Weapon/B_Weapon.h"
+
+#include "Components/SkeletalMeshComponent.h"
+
+AB_Weapon::AB_Weapon()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+	bNetUseOwnerRelevancy = true;
+	
+	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>("Mesh1P");
+	Mesh1P->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::OnlyTickPoseWhenRendered;
+	Mesh1P->bReceivesDecals = false;
+	Mesh1P->CastShadow = false;
+	Mesh1P->SetHiddenInGame(true);
+	SetRootComponent(Mesh1P);
+	
+	Mesh3P = CreateDefaultSubobject<USkeletalMeshComponent>("Mesh3P");
+	Mesh3P->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::OnlyTickPoseWhenRendered;
+	Mesh3P->bReceivesDecals = false;
+	Mesh3P->SetupAttachment(Mesh1P);
+	Mesh3P->SetHiddenInGame(true);
+}
+
+void AB_Weapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
---
+++ Source/Belter/Public/Weapon/B_Weapon.h
@@ -0,0 +1,33 @@
+﻿// Copyright Eduard Ciofu
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "B_Weapon.generated.h"
+
+UCLASS()
+class BELTER_API AB_Weapon : public AActor
+{
+	GENERATED_BODY()
+
+public:
+
+	AB_Weapon();
+	
+	USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
+	USkeletalMeshComponent* GetMesh3P() const { return Mesh3P; }
+	
+protected:
+
+	virtual void BeginPlay() override;
+
+private:
+	
+	// Weapon Mesh for 1st person view
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USkeletalMeshComponent> Mesh1P;
+	// Weapon Mesh for 3rd person view
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USkeletalMeshComponent> Mesh3P;
+};
